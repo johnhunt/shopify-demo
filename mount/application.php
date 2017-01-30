b05e025bc41d44f816b80dd97d0b1526328675dc
John Hunt <john@boilerjuice.com>
2017-01-30 21:25:22 +0000

    Added pages controller to routes for serving pages

--- mount/application.php
+++ mount/application.php
@@ -3,10 +3,15 @@ require_once('vendor/autoload.php');
 
 $app = new Slim\App();
 
+// Gather dependencies.. on a bigger app I'd probably use a dependency container
+/*
+$guzzleClient = new GuzzleHttp\Client();
+$config = new \Configula\Config();
+$smarty = new Smarty();
+
+ */
 // Add route callbacks
-$app->get('/shop-demo', function ($request, $response, $args) {
-    return $response->withStatus(200)->write('Hello World!');
-});
+$app->get('/shop-demo', 'Controllers\PagesController:index');
 
 // Run application
 $app->run();
