20a553dff86e4a2069a45ea8dd95c760e1738cfb
John Hunt <john@boilerjuice.com>
2017-01-30 22:39:20 +0000

    Pages controller now using smarty template

--- mount/src/Controllers/PagesController.php
+++ mount/src/Controllers/PagesController.php
@@ -7,13 +7,17 @@ use Slim\Container;
 
 class PagesController
 {
+    /** @var \Smarty $smarty */
+    private $smarty;
+
     public function __construct(Container $container)
     {
         $this->container = $container;
+        $this->smarty = $container['smarty'];
     }
 
     public function index(Request $request, Response $response, $params)
     {
-        die($this->container['test']);
+        return $response->withStatus(200)->write($this->smarty->fetch('index.tpl'));
     }
 }
