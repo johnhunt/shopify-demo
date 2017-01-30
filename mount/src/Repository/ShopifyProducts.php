81101f3559239704f56f005684bfaf56ea75160c
John Hunt <john@boilerjuice.com>
2017-01-30 21:26:33 +0000

    Added shopify product repository

---
+++ mount/src/Repository/ShopifyProducts.php
@@ -0,0 +1,18 @@
+<?php
+namespace Repository;
+
+use Entities\Product;
+use GuzzleHttp\Client;
+
+class ShopifyProducts
+{
+    public function __construct(Client $guzzle)
+    {
+
+    }
+
+    public function addProduct(Product $product)
+    {
+
+    }
+}
