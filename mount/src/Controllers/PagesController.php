9f7c89e99b38fc5ca9b264f2c7662ebf4326010a
John Hunt <john@boilerjuice.com>
2017-01-30 21:25:38 +0000

    Added simple pages controller

---
+++ mount/src/Controllers/PagesController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class PagesController
+{
+    public function index(Request $request, Response $response, $params)
+    {
+        die('yup');
+    }
+}
