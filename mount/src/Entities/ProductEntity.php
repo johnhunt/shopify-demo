7d8a409ef1516ec2f759cce56557c905e14e91c8
John Hunt <john@boilerjuice.com>
2017-01-31 23:10:08 +0000

    Fleshed out the product entity class

--- mount/src/Entities/ProductEntity.php
+++ mount/src/Entities/ProductEntity.php
@@ -1,14 +1,72 @@
 <?php
 namespace Entities;
 
-class Product implements ProductInterface
+class ProductEntity implements ProductInterface
 {
+    /**
+     * @var
+     */
+    private $title;
+    /**
+     * @var
+     */
+    private $body;
+    /**
+     * @var
+     */
+    private $vendor;
+    /**
+     * @var
+     */
+    private $productType;
+    /**
+     * @var
+     */
+    private $variants;
+    /**
+     * @var
+     */
+    private $sku;
+    /**
+     * @var
+     */
+    private $price;
+
+    /**
+     * Product constructor - loads data into the entity
+     *
+     * @param $title
+     * @param $body
+     * @param $vendor
+     * @param $productType
+     * @param $variants
+     */
+    public function __construct($sku, $price, $title, $body, $vendor, $productType)
+    {
+        $this->title = $title;
+        $this->body = $body;
+        $this->vendor = $vendor;
+        $this->productType = $productType;
+        $this->sku = $sku;
+        $this->price = $price;
+    }
+
+    public function sku()
+    {
+        return $this->sku;
+    }
+
+    public function price()
+    {
+        return $this->price;
+    }
+
     /**
      * @return string The product title
      */
     public function title()
     {
-        // TODO: Implement title() method.
+        return $this->title;
     }
 
     /**
@@ -16,7 +74,7 @@ class Product implements ProductInterface
      */
     public function body()
     {
-        // TODO: Implement body() method.
+        return $this->body;
     }
 
     /**
@@ -24,7 +82,7 @@ class Product implements ProductInterface
      */
     public function vendor()
     {
-        // TODO: Implement vendor() method.
+        return $this->vendor;
     }
 
     /**
@@ -32,15 +90,6 @@ class Product implements ProductInterface
      */
     public function productType()
     {
-        // TODO: Implement productType() method.
+        return $this->productType;
     }
-
-    /**
-     * @return array The product variants (in shopify format)
-     */
-    public function variants()
-    {
-        // TODO: Implement variants() method.
-    }
-
 }
