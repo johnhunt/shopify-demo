05a1d052f5274b743b7f1488efe19c7e3c1b1be6
John Hunt <john@boilerjuice.com>
2017-01-31 23:09:40 +0000

    Added shopify url to config as well as API key/password & secret

--- mount/config/config.php
+++ mount/config/config.php
@@ -1,4 +1,9 @@
 <?php
 $config = [
-  'siteName' => "John's amazing shop site"
+    'siteName' => "John's amazing shop site",
+    // The following should be added to your config.local.php beside this file with the real values
+    'shopifyUrl' => 'https://test-urg.myshopify.com',
+    'apiKey' => '',
+    'apiPassword' => '',
+    'sharedSecret' => ''
 ];
